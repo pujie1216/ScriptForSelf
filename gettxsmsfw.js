1dad91b978d4bb2fd8b1e0762930fea4cbce0da5
pujie1216 <21360598+pujie1216@users.noreply.github.com>
2025-05-17 18:37:07 +0800

    兼容Loon插件方式,添加serverchan通知方式

--- gettxsmsfw.js
+++ gettxsmsfw.js
@@ -1,9 +1,33 @@
-let notifyl = [];
+let notify, tokenetc, allsms = true, regexstr = '码|碼|code|\\d{4,}'; //正则匹配转发特定的短信,可以按需修改正则,需要转义
 try {
-    notifyl = $argument.split('||'); //token等使用传入参数,代码无需写死,更加灵活
+    if (typeof $argument == 'string') { //token等使用传入参数,代码无需写死,更加灵活
+        const notifyl = $argument.split('||');
+        notify = notifyl[0].trim();
+        tokenetc = notifyl[1].trim();
+    } else {
+        switch ($argument.notify) {
+        case '钉钉':
+            notify = '0';
+            break;
+        case 'Server酱':
+            notify = '1';
+            break;
+        default:
+            notify = '-1';
+        }
+        tokenetc = $argument.tokenetc;
+        allsms = $argument.allsms;
+        regexstr = $argument.regexstr;
+    }
 } catch {
-    console.log('没有设置参数,停止运行');
+    $notification.post($script.name, '', '参数不正确,停止运行');
 }
+
+if (tokenetc) {
+    main();
+}
+
+$done();
 
 function main() {
     let txreqbody = '',
@@ -15,32 +39,31 @@ function main() {
     }
     const smso = JSON.parse(txreqbody);
     const sms = smso?.query?.message?.text ?? '获取TX转发短信失败';
-    //forward = /码|碼|code|\d{4,}/.test(sms); //正则匹配转发特定的短信,删除该行最前面的两斜杠以开启,可以按需修改正则
-	
-	//使用switch语句,预留自定义转发方式,如有需要,自行参考对应的API文档编写代码
+    if (!allsms) {
+        forward = new RegExp(regexstr).test(sms);
+    }
+
+    //使用switch语句,预留自定义通知方式,如有需要,自行参考对应的API文档编写代码
     if (forward) {
-        switch (notifyl[0].trim()) {
+        switch (notify) {
         case '0':
-            dtnotification(notifyl[1], sms);
+            dtnotification(tokenetc, sms);
+            break;
+        case '1':
+            serverchannoti(tokenetc, sms);
             break;
         default:
-            $notification.post('参数错误,停止运行');
+            $notification.post($script.name, '', '参数不正确,停止运行');
         }
     }
 }
-
-if (notifyl.length > 0) {
-    main();
-}
-
-$done();
 
 function postmsg(requrl, reqbody) {
     const reqparams = {
         url: requrl,
         headers: {
             'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/136.0.0.0 Safari/537.36 Edg/136.0.0.0',
-            'Content-Type': 'application/json'
+            'Content-Type': 'application/json;charset=utf-8'
         },
         body: reqbody,
         timeout: 5000
@@ -54,4 +77,12 @@ function dtnotification(kwactk, sms) {
     const dtwebhookurl = 'https://oapi.dingtalk.com/robot/send?access_token=' + kwactkl[1].trim();
     const reqbody = `{"msgtype":"text","text":{"content":"${dtkeyword}\n${sms}"}}`;
     postmsg(dtwebhookurl, reqbody);
+}
+
+function serverchannoti(sendkey, sms) {
+    const serverchanurl = String(sendkey).startsWith('sctp')
+         ? `https://${sendkey.match(/^sctp(\d+)t/)[1]}.push.ft07.com/send/${sendkey}.send`
+         : `https://sctapi.ftqq.com/${sendkey}.send`;
+    const reqbody = `{"title":"TX短信转发","desp":"${sms}"}`;
+    postmsg(serverchanurl, reqbody);
 }
